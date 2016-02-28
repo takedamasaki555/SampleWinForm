52689444bf61b6ab7ffbe116202818947c41946f
武田正樹 <takeda_masaki@hotmail.com>
2016-02-28 22:45:13 +0900

    Initial

---
+++ SampleWinForm/SampleWinForm/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace SampleWinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNumber1 = new System.Windows.Forms.TextBox();
+            this.txtNumber2 = new System.Windows.Forms.TextBox();
+            this.txtAnswer = new System.Windows.Forms.TextBox();
+            this.cmbOperator = new System.Windows.Forms.ComboBox();
+            this.btnEqual = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNumber1
+            // 
+            this.txtNumber1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtNumber1.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.txtNumber1.Location = new System.Drawing.Point(14, 13);
+            this.txtNumber1.MaxLength = 4;
+            this.txtNumber1.Name = "txtNumber1";
+            this.txtNumber1.Size = new System.Drawing.Size(80, 19);
+            this.txtNumber1.TabIndex = 0;
+            this.txtNumber1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNumber_KeyPress);
+            // 
+            // txtNumber2
+            // 
+            this.txtNumber2.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.txtNumber2.Location = new System.Drawing.Point(146, 13);
+            this.txtNumber2.MaxLength = 4;
+            this.txtNumber2.Name = "txtNumber2";
+            this.txtNumber2.Size = new System.Drawing.Size(80, 19);
+            this.txtNumber2.TabIndex = 1;
+            this.txtNumber2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNumber_KeyPress);
+            // 
+            // txtAnswer
+            // 
+            this.txtAnswer.Location = new System.Drawing.Point(258, 12);
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.ReadOnly = true;
+            this.txtAnswer.Size = new System.Drawing.Size(120, 19);
+            this.txtAnswer.TabIndex = 2;
+            // 
+            // cmbOperator
+            // 
+            this.cmbOperator.FormattingEnabled = true;
+            this.cmbOperator.Items.AddRange(new object[] {
+            "＋",
+            "－",
+            "×",
+            "÷"});
+            this.cmbOperator.Location = new System.Drawing.Point(100, 12);
+            this.cmbOperator.Name = "cmbOperator";
+            this.cmbOperator.Size = new System.Drawing.Size(40, 20);
+            this.cmbOperator.TabIndex = 3;
+            // 
+            // btnEqual
+            // 
+            this.btnEqual.Location = new System.Drawing.Point(232, 10);
+            this.btnEqual.Name = "btnEqual";
+            this.btnEqual.Size = new System.Drawing.Size(20, 23);
+            this.btnEqual.TabIndex = 4;
+            this.btnEqual.Text = "＝";
+            this.btnEqual.UseVisualStyleBackColor = true;
+            this.btnEqual.Click += new System.EventHandler(this.btnEqual_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 44);
+            this.Controls.Add(this.btnEqual);
+            this.Controls.Add(this.cmbOperator);
+            this.Controls.Add(this.txtAnswer);
+            this.Controls.Add(this.txtNumber2);
+            this.Controls.Add(this.txtNumber1);
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNumber1;
+        private System.Windows.Forms.TextBox txtNumber2;
+        private System.Windows.Forms.TextBox txtAnswer;
+        private System.Windows.Forms.Button btnEqual;
+        private System.Windows.Forms.ComboBox cmbOperator;
+    }
+}
+
---
+++ SampleWinForm/SampleWinForm/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SampleWinForm
+{
+    public partial class Form1 : Form
+    {
+        #region Constants
+        private const char ZERO = '0';
+        private const char NINE = '9';
+        private const char BACKSPACE = '\b';
+        private const string MSG_OPERALTOR_IS_NOT_SELECTED = "計算方法を選択してください";
+        private const string MSG_NUMBER1_IS_NOT_INTEGER = "一つ目の入力が数字ではありません";
+        private const string MSG_NUMBER2_IS_NOT_INTEGER = "二つ目の入力が数字ではありません";
+        private const string MSG_CANNOT_DIVIDE_BY_ZERO = "ゼロで割ることはできません";
+        #endregion
+
+        #region Fields
+        private Calculator calculator = new Calculator();
+        #endregion
+
+        #region Initializers
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region EventHandlers
+        private void txtNumber_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = isCancelKeyPress(e.KeyChar);
+        }
+
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+            if (isEmptyOperator()) return;
+            if (isInvalidNumber1()) return;
+            if (isInvalidNumber2()) return;
+
+            txtAnswer.Text = calculate(int.Parse(txtNumber1.Text), int.Parse(txtNumber2.Text));
+        }
+        #endregion
+
+        #region Validations
+        private bool isCancelKeyPress(char key)
+        {
+            return (key < ZERO || key > NINE) && key != BACKSPACE;
+        }
+
+        private bool isEmptyOperator()
+        {
+            if (cmbOperator.SelectedIndex >= 0) return false;
+            MessageBox.Show(MSG_OPERALTOR_IS_NOT_SELECTED);
+            return true;
+        }
+
+        private bool isInvalidNumber1()
+        {
+            var number1 = 0;
+            if (int.TryParse(txtNumber1.Text, out number1)) return false;
+            MessageBox.Show(MSG_NUMBER1_IS_NOT_INTEGER);
+            return true;
+        }
+
+        private bool isInvalidNumber2()
+        {
+            var number2 = 0;
+            var isInteger = int.TryParse(txtNumber2.Text, out number2);
+
+            if (!isInteger)
+            {
+                MessageBox.Show(MSG_NUMBER2_IS_NOT_INTEGER);
+                return true;
+            }
+            else if (cmbOperator.SelectedIndex == 3 && number2 == 0)
+            {
+                MessageBox.Show(MSG_CANNOT_DIVIDE_BY_ZERO);
+                return true;
+            }
+            return false;
+        }
+        #endregion
+
+        #region calculations
+        private string calculate(int number1, int number2)
+        {
+            switch (cmbOperator.SelectedIndex)
+            {
+                case 0:
+                    return calculator.Add(number1, number2).ToString();
+                case 1:
+                    return calculator.Subtract(number1, number2).ToString();
+                case 2:
+                    return calculator.Multiply(number1, number2).ToString();
+                case 3:
+                    return calculator.Divide(number1, number2).ToString();
+                default:
+                    return string.Empty;
+            }
+        }
+        #endregion
+    }
+}
